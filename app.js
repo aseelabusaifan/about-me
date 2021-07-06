936ce66ecce2a386797616c22cb4a6e376e3dcb2
aseelabusaifan <adeelmohammsd@gmail.com>
2021-07-06 21:14:05 +0300

    add

--- app.js
+++ app.js
@@ -6,4 +6,19 @@ edu = edu .toupperCase();
 switch (edu){
     case 'yarmouk':
         alert ('welcome to our university') ; }
-    
+
+
+     let favourite drinks =prompt('is acofee my favourite drinks');
+
+
+     if (favourite drinks==='yes'|| favourite drinks=== 'y'){
+         console log (favourite drinks:,favourite drinks);
+         alert(good chooes);
+
+     }else if (favourite drinks ==='no'||favourite drinks 'n');
+     alert ('its not my favourite ');
+     
+    }else{
+        alert('pleas only write yes/y or no/n')
+
+     }
