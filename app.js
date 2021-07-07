068df833bf2c6ff3bfbb8cc11501d735ed3b2244
HaninHaidrah <haneenhadey@gmail.com>
2021-07-07 15:29:26 +0300

    adding fuction

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
 'use strict' 
 
-let edu = prompet (' what is your university');
-console.log ( 'befor', edu );
-edu = edu .toupperCase();
-switch (edu){
-    case 'yarmouk':
-        alert ('welcome to our university') ; }
+function question1() {
+    if (edu === 'yes' || edu === 'y') {
+
+        alert('yes you got it');
+        score++;
+    }
+
+    else if (edu === 'no' || edu === 'n') {
+        alert('try again');
+    }
+}
+
+question1();
+
+let favouritedrinks = prompt('is acofee my favourite drinks');
 
 
-     let favouritedrinks =prompt('is acofee my favourite drinks');
+function question2() {
+    if (favouritedrinks === 'yes' || favouritedrinks === 'y') {
+        // console log(favouritedrinks:, favouritedrinks);
 
+        alert('good chooes');
+        score++;
+    }
+    else if (favouritedrinks === 'no' || favouritedrinks === 'n') {
 
-     if (favouritedrinks==='yes'|| favouritedrinks=== 'y'){
-         console log (favouritedrinks:,favouritedrinks);
-         alert (good chooes);
+        alert('its not my favourite ');
 
-     }else if (favouritedrinks ==='no'||favouritedrinks 'n');
-     alert ('its not my favourite ');
+    }
 
-    }else{
+    else {
         alert('pleas only write yes/y or no/n');
 
-     }
+    }
+
+}
+
+
+question2();
