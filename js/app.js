96f204433a0cf4a76b6d6a35cf071be6755757a2
aseelabusaifan <adeelmohammsd@gmail.com>
2021-07-08 15:44:04 +0300

    add more

--- js/app.js
+++ js/app.js
@@ -1,104 +1,104 @@
-// 'use strict' 
-// let score = 0;
-//  let edu=prompt('did i study  at yarmouk uni ')
-// function question1() {
-//     if (edu === 'yes' || edu === 'y') {
+'use strict' 
+let score = 0;
+ let edu=prompt('did i study  at yarmouk uni ')
+function question1() {
+    if (edu === 'yes' || edu === 'y') {
 
-//         alert('yes you got it');
-//         score++;
-//     }
+        alert('yes you got it');
+        score++;
+    }
 
-//     else if (edu === 'no' || edu === 'n') {
-//         alert('try again');
-//     }
-// }
+    else if (edu === 'no' || edu === 'n') {
+        alert('try again');
+    }
+}
 
-// question1();
+question1();
 
-// let favouritedrinks = prompt('is acofee my favourite drinks');
-
-
-// function question2() {
-//     if (favouritedrinks === 'yes' || favouritedrinks === 'y') {
-//         // console log(favouritedrinks:, favouritedrinks);
-
-//         alert('good chooes');
-//         score++;
-//     }
-//     else if (favouritedrinks === 'no' || favouritedrinks === 'n') {
-
-//         alert('its not my favourite ');
-
-//     }
-
-//     else {
-//         alert('pleas only write yes/y or no/n');
-
-//     }
-
-// }
+let favouritedrinks = prompt('is acofee my favourite drinks');
 
 
-// question2();
+function question2() {
+    if (favouritedrinks === 'yes' || favouritedrinks === 'y') {
+        // console log(favouritedrinks:, favouritedrinks);
+
+        alert('good chooes');
+        score++;
+    }
+    else if (favouritedrinks === 'no' || favouritedrinks === 'n') {
+
+        alert('its not my favourite ');
+
+    }
+
+    else {
+        alert('pleas only write yes/y or no/n');
+
+    }
+
+}
 
 
-// let study = prompt(' Did I study public adminstration??');
-// // console.log ( 'befor', edu );
-// study = study.toLowerCase();
-
-// function question3() {
-//     if (study === 'yes' || study === 'y') {
-
-//         alert('yes you got it');
-//         score++;
-//     }
-
-//     else if (study === 'no' || study === 'n') {
-//         alert('try again');
-//     }
-// }
-//     question3();
+question2();
 
 
-//     let work = prompt(' Did I work for Amazon before?');
-//     // console.log ( 'befor', edu );
-//     work = work.toLowerCase();
+let study = prompt(' Did I study public adminstration??');
+// console.log ( 'befor', edu );
+study = study.toLowerCase();
+
+function question3() {
+    if (study === 'yes' || study === 'y') {
+
+        alert('yes you got it');
+        score++;
+    }
+
+    else if (study === 'no' || study === 'n') {
+        alert('try again');
+    }
+}
+    question3();
+
+
+    let work = prompt(' Did I work for Amazon before?');
+    // console.log ( 'befor', edu );
+    work = work.toLowerCase();
     
-//     function question4() {
-//         if (work === 'yes' || work === 'y') {
+    function question4() {
+        if (work === 'yes' || work === 'y') {
     
-//             alert('try again');
-//         }
+            alert('try again');
+        }
     
-//         else if (work === 'no' || work === 'n') {
+        else if (work === 'no' || work === 'n') {
     
-//             alert('yes you got it');
-//             score++;
-//         }
-//     }
+            alert('yes you got it');
+            score++;
+        }
+    }
 
-//     question4();
+    question4();
 
-//     let exp = prompt(' Did I have experience in markting');
-//     // console.log ( 'befor', edu );
-//     exp = exp.toLowerCase();
-
-
-//     function question5(){
-
-//     if (exp === 'yes' || exp === 'y') {
-
-//         alert('yes you got it');
-//         score++;
-//     }
-
-//     else if (exp === 'no' || exp === 'n') {
-//         alert('try again');
-//     }
-// }
+    let exp = prompt(' Did I have experience in markting');
+    // console.log ( 'befor', edu );
+    exp = exp.toLowerCase();
 
 
-// question5();
+    function question5(){
+
+    if (exp === 'yes' || exp === 'y') {
+
+        alert('yes you got it');
+        score++;
+    }
+
+    else if (exp === 'no' || exp === 'n') {
+        alert('try again');
+    }
+}
+
+
+question5();
 
 
 
@@ -122,6 +122,49 @@ if (num==='4'){
     num= prompt(' what my luck num is');
 
 }
+
+
+
+
+let hr = prompt(' Did I study the hr??');
+
+study = study.toLowerCase();
+
+function question6() {
+    if (hr === 'yes' || hr === 'y') {
+
+        alert('yes you got it');
+        score++;
+    }
+
+    else if ( hr === 'no' || hr === 'n') {
+        alert('try again');
+    }
+}
+    question6();
+
+
+
+    
+let  sport = prompt('Is my favorite sport walking? ??');
+
+sport  =  sport.toLowerCase();
+
+function question7() {
+    if (sport === 'yes' || sport  === 'y') {
+
+        alert('yes you got it');
+        score++;
+    }
+
+    else if (  sport === 'no' ||  sport=== 'n') {
+        alert('try again');
+    }
+}
+    question7();
+
+
+
 
 
 
